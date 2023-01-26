1fc1ec68d5a907a2df4fd47406c5c977efc8de53
DinnerKang <jenhyuk0318@gmail.com>
2023-01-27 01:17:23 +0900

    갤러리 수정

--- gallery.js
+++ gallery.js
@@ -13,12 +13,15 @@ const changeTag = (index) => {
   setImages();
 };
 
-const showMoreImages = (url) => {
+const showMoreImages = (url, idx) => {
   document.querySelector('body').style.overflow = 'hidden';
   modal.style.display = 'flex';
+  checkArrow(idx);
+
   imgArea.innerHTML = '';
   const img = document.createElement('div');
-  img.innerHTML = `<img src="${url}" alt="결혼사진" />`;
+  img.innerHTML = `<img class="gallery-img" src="${url}" alt="결혼사진" />`;
+  selectedImg = url;
   imgArea.append(img);
 };
 
@@ -27,17 +30,42 @@ const closeModal = () => {
   document.querySelector('body').style.overflow = 'auto';
 };
 
+const moveImg = (direction) => {
+  const index = imageList.findIndex(i => i.url === selectedImg);
+  const nextImg = imageList[index + direction];
+
+  imgArea.innerHTML = '';
+
+  const img = document.createElement('div');
+  img.innerHTML = `<img class="gallery-img" src="${nextImg.url}" alt="결혼사진"  />`;
+  selectedImg = nextImg.url;
+  checkArrow(index + direction);
+  imgArea.append(img);
+};
+
+const checkArrow = (idx) => {
+  if (idx === 0) {
+    document.getElementsByClassName('left')[0].classList.add('hidden');
+  } else {
+    document.getElementsByClassName('left')[0].classList.remove('hidden');
+  }
+  if (idx === imageList.length - 1) {
+    document.getElementsByClassName('right')[0].classList.add('hidden');
+  } else {
+    document.getElementsByClassName('right')[0].classList.remove('hidden');
+  }
+}
 
 const setImages = () => {
   gallery.innerHTML = '';
 
-  imageList.forEach((item,) => {
+  imageList.forEach((item, idx) => {
     if (selectedTag === 'all' || item.tag === selectedTag) {
       const list = document.createElement('li');
       list.innerHTML = `<img class="imgs" src="${item.url}" alt="결혼사진" />`;
 
       list.addEventListener('click', () => {
-        showMoreImages(item.url);
+        showMoreImages(item.url, idx);
       });
 
       gallery.append(list);
